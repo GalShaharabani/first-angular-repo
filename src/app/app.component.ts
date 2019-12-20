bd5c4d6c0ff4b8c619c52d33ea2e5338aadcf046
Gal shaharabani <galshar1@gmail.com>
2019-12-20 18:10:40 +0200

    improved angular site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  date:Date;
+
+  ngOnInit(): void {
+  this.date=new Date();   
+
+  }
   title = 'FirstAngularSite';
+  id='';
+  firstName ='';
+  lastName='';
+
+ public popup() {
+     alert("ID= "+this.id+", First name= "+this.firstName+", Last name= "+this.lastName);
+  }
+
 }
+
+
